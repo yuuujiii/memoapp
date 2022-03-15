6999649d165ef0be17a57ed3a55c913cd0c58bd2
EC2 Default User <ec2-user@ip-172-31-43-93.ec2.internal>
2022-03-15 12:52:31 +0000

    修正版

--- memo.rb
+++ memo.rb
@@ -9,15 +9,17 @@ if memo_type == "1"
   title = gets.chomp.to_s
   CSV.open("#{title}.csv","w") do |csv|
     puts "-------メモしたい内容の入力してください-------"
-    create = gets.chomp.to_s
-    csv << [create]
+    puts "-------入力後control+Dを押してください-------"
+    create = readlines(chomp: true)
+    csv << create
   end
 elsif memo_type == "2"
   puts "--編集したいファイルを拡張子を除いて入力してください--"
   search = gets.chomp.to_s
   CSV.open("#{search}.csv","w") do |csv|
     puts "-------上書きしたい内容の入力してください-------"
-    create = gets.chomp.to_s
-    csv << [create]
+    puts "-------入力後control+Dを押してください-------"
+    create = readlines(chomp: true)
+    csv << create
   end
 end
